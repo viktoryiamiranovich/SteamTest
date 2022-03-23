b11e4faaa647817ef4cf045581f7cede7dbb3d83
Vika Mironovich <vikamironovich@mail.ru>
2022-03-23 18:51:24 +0300

    for Jenkins

--- src/test/java/framework/BaseTest.java
+++ src/test/java/framework/BaseTest.java
@@ -2,9 +2,11 @@ package framework;
 
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.apache.log4j.Logger;
 
 public abstract class BaseTest extends BaseEntity {
 
+    public static final Logger log = Logger.getLogger(BaseTest.class);
 
     @BeforeTest (description = "opening browser")
     public void driverStart(){
@@ -14,5 +16,6 @@ public abstract class BaseTest extends BaseEntity {
     @AfterTest
     public void testCompletion(){
         tearDown();
+        log.info("Close browser");
     }
 }
--- src/test/java/steam/test/SteamTest.java
+++ src/test/java/steam/test/SteamTest.java
@@ -10,22 +10,30 @@ public class SteamTest extends BaseTest {
     @Test(description = "Select action game and download Steam")
     public void steamDownloadCheck() {
 
+        log.info("Open Steam");
         HomePage homePage = new HomePage();
+        log.info("Select language");
         homePage.languageMenu.chooseLanguage();
+        log.info("Go to Action page");
         homePage.gamesMenu.navigateMenu(MainMenu.Menu.CATEGORY, MainMenu.Games.ACTION);
 
         ActionPage actionPage = new ActionPage();
+        log.info("Looking for game with maximum discount");
         actionPage.lookingForGameWithBiggestSale();
+        log.info("Open game page with maximum discount");
 
         AgePage checkAge = new AgePage();
         if (checkAge.isAgeCheckPageOpened()) {
+            log.info("Check age");
             checkAge.ageCheckActions();
         }
 
         GamePage gamePage = new GamePage();
+        log.info("Go to the Download Steam page");
         gamePage.installSteamBtnClick();
 
         SteamDownloadPage steamDownload = new SteamDownloadPage();
+        log.info("Download Steam");
         steamDownload.downloadSteam();
     }
 }
